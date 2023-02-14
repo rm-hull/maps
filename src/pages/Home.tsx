791a705e82da1f62258b670772e66151864c5d98
Richard Hull <rm_hull@yahoo.co.uk>
2023-02-14 00:05:41 +0000

    Use road maps layer when zoom level gets too high

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,14 @@ import "proj4leaflet";
 // import { useGeolocated } from "react-geolocated";
 import { MapContainer, TileLayer } from "react-leaflet";
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 // Setup the EPSG:27700 (British National Grid) projection.
 const crs = new L.Proj.CRS(
   "EPSG:27700",
   "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs",
   {
-    resolutions: [896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75],
+    resolutions: [896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75, 0.875, 0.4375, 0.21875, 0.109375],
     origin: [-238375.0, 1376256.0],
   }
 );
@@ -30,7 +32,7 @@ export default function Home(): JSX.Element {
       crs={crs}
       zoom={7}
       minZoom={0}
-      maxZoom={9}
+      maxZoom={13}
       // center={[coords?.latitude ?? 51.505, coords?.longitude ?? -0.09]}
       center={transformCoords([337297, 503695])}
       maxBounds={[transformCoords([-238375.0, 0.0]), transformCoords([900000.0, 1376256.0])]}
@@ -38,10 +40,8 @@ export default function Home(): JSX.Element {
       style={{ width: "100vw", height: "100vh" }}
       attributionControl={false}
     >
-      <TileLayer
-        // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://api.os.uk/maps/raster/v1/zxy/Leisure_27700/{z}/{x}/{y}.png?key=RhZJhHjGDTpDLotkGOXacbvDR7T4eWUa"
-      />
+      <TileLayer url={`https://api.os.uk/maps/raster/v1/zxy/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`} maxZoom={9} />
+      <TileLayer url={`https://api.os.uk/maps/raster/v1/zxy/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`} minZoom={10} />
     </MapContainer>
   );
 }
