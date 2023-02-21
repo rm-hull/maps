007ffaa80c2a2d5a01311c0b3087634f0c60e31c
Richard Hull <rm_hull@yahoo.co.uk>
2023-02-21 23:51:52 +0000

    Activate location beacon on clicking button; lasts for 2 mins

--- src/components/LocationMarker.tsx
+++ src/components/LocationMarker.tsx
@@ -1,54 +1,84 @@
 import { Button } from "@chakra-ui/react";
 import { LatLng } from "leaflet";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { IoMdLocate } from "react-icons/io";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
 import Control from "react-leaflet-custom-control";
 import { useInterval } from "react-use";
 
-type LocationMarkerProps = {
-  onChange?: (latLng: LatLng) => void;
+type LocationDetails = {
+  position?: LatLng;
+  lastUpdated: number;
+  active: boolean;
+  pending: boolean;
+  cancelTimerId?: NodeJS.Timeout;
 };
 
-export default function LocationMarker({ onChange }: LocationMarkerProps) {
-  const [position, setPosition] = useState<LatLng | null>(null);
+export default function LocationMarker() {
+  const [locationDetails, setLocationDetails] = useState<LocationDetails>({
+    active: false,
+    pending: true,
+    lastUpdated: Date.now(),
+  });
   const map = useMapEvents({
     locationfound(e) {
-      setPosition(e.latlng);
-      onChange?.(e.latlng);
+      if (!locationDetails.pending) {
+        map.flyTo(e.latlng, map.getZoom());
+      }
+
+      setLocationDetails((prev) => ({
+        ...prev,
+        pending: false,
+        position: e.latlng,
+        lastUpdated: Date.now(),
+      }));
     },
   });
 
-  useEffect(() => {
-    map.locate();
-  }, [map]);
-
   useInterval(() => {
-    map.locate();
-  }, 5_000);
+    if (locationDetails.active) {
+      map.locate();
+    }
+  }, 10_000);
 
-  const handleLocate = () => {
-    position && map.flyTo(position, map.getZoom());
+  const activate = () => {
+    map.locate();
+
+    clearTimeout(locationDetails.cancelTimerId);
+    const timerId = setTimeout(() => {
+      setLocationDetails((prev) => ({ ...prev, active: false }));
+    }, 120_000);
+
+    setLocationDetails((prev) => ({
+      ...prev,
+      pending: true,
+      active: true,
+      cancelTimerId: timerId,
+    }));
   };
 
   return (
     <>
-      {position && (
-        <Marker position={position}>
-          <Popup>You are here: {position.toString()}</Popup>
+      {locationDetails.position && (
+        <Marker
+          position={locationDetails.position}
+          opacity={locationDetails.active && !locationDetails.pending ? 1 : 0.6}
+        >
+          <Popup>
+            <p>You are here: {locationDetails.position.toString()}</p>
+          </Popup>
         </Marker>
       )}
       <Control prepend position="topright">
         <Button
           background="white"
           variant="outline"
-          onClick={handleLocate}
-          disabled={!position}
+          onClick={activate}
           padding={0}
           borderWidth={2}
           borderColor="rgba(0,0,0,0.25)"
           fontSize="1.5rem"
-          color="rgba(0,0,0,0.5)"
+          color={locationDetails.active ? "rgba(240,0,0,0.5)" : "rgba(0,0,0,0.5)"}
           borderRadius={5}
           size="lg"
         >
