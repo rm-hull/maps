b348e75e9d5dddda71f4b34f13a524c153959a33
Richard Hull <rm_hull@yahoo.co.uk>
2024-07-07 18:03:22 +0100

    Update web-vitals on console.debug

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./App";
 import { ErrorFallback } from "./components/ErrorFallback";
 import { SettingsProvider } from "./components/settings/SettingsProvider";
-import reportWebVitals from "./reportWebVitals";
+import { reportWebVitals } from "./reportWebVitals";
 
 if (import.meta.env.VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID !== undefined) {
   ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID as string);
@@ -46,4 +46,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.debug);
