237662063413a4708e498fddd2cbd101216b4ca0
Richard Hull <rm_hull@yahoo.co.uk>
2025-06-06 14:54:22 +0100

    Add API key

--- src/services/gpsRoutes/index.ts
+++ src/services/gpsRoutes/index.ts
@@ -7,9 +7,17 @@ if (API_URL === undefined) {
   throw Error("No GPS Routes API URL specified");
 }
 
+const API_KEY = import.meta.env.VITE_GPS_ROUTES_API_KEY as string;
+if (API_KEY === undefined) {
+  throw Error("No GPS Routes API Key specified");
+}
+
 const client = axios.create({
   baseURL: API_URL,
   timeout: 10000,
+  headers: {
+    "X-API-Key": API_KEY,
+  },
 });
 
 export const fetchGpsRoutes = async (bounds: LatLngBounds, truncateText: boolean): Promise<SearchResponse> => {
