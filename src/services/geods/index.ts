fdbd9c96641733411db881eda5756fcd8f89d460
Richard Hull <rm_hull@yahoo.co.uk>
2025-06-16 10:37:27 +0100

    Apply suggestions from code review

--- src/services/geods/index.ts
+++ src/services/geods/index.ts
@@ -15,11 +15,11 @@ const client = axios.create({
 
 export const fetchGeodsPOI = async (bounds: LatLngBounds): Promise<SearchResponse> => {
   if (API_URL === undefined) {
-    throw Error("No POI API URL specified");
+    throw Error("No GeoDS POI API URL specified");
   }
 
   // if (API_KEY === undefined) {
-  //   throw Error("No GPS Routes API Key specified");
+  //   throw Error("No GeoDS POI API Key specified");
   // }
 
   const resp = await client.get<SearchResponse>("/v1/poi/search", {
