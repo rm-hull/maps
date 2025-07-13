4e2651b19f6bdaa9c6e1f806bee6cc3d0513524c
Richard Hull <rm_hull@yahoo.co.uk>
2025-07-13 15:53:34 +0100

    Refactor custom layers controller (#618)
    * Refactor custom layers controller
    * Remove unnecessary return type
    * Align min zoom level to prior values

--- src/components/map/layers/GeographLayer.tsx
+++ src/components/map/layers/GeographLayer.tsx
@@ -1,28 +1,26 @@
-import { LayerGroup, Marker, useMap, useMapEvents } from "react-leaflet";
-import { type LatLng } from "leaflet";
+import { Marker, useMap } from "react-leaflet";
+import { LatLngBounds } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import ResultPopup from "../ResultPopup";
 import { useErrorToast } from "../../../hooks/useErrorToast";
-import { useGeneralSettings } from "../../../hooks/useGeneralSettings";
 import { useGeograph } from "../../../hooks/useGeograph";
-import { useState } from "react";
 
-interface ImagesProps {
-  latLng: LatLng;
-  distance: number;
+interface GeographLayerProps {
+  bounds: LatLngBounds;
 }
 
-function Images({ latLng, distance }: ImagesProps) {
-  const { data, error } = useGeograph(latLng, distance / 1000.0);
-  useErrorToast("geograph-error", "Error loading Geograph images", error);
+export function GeographLayer({ bounds }: GeographLayerProps) {
+  const map = useMap();
 
-  if (data === undefined || error !== undefined) {
-    return null;
-  }
+  const center = bounds.getCenter();
+  const distance = map.distance(bounds.getSouthWest(), bounds.getNorthEast());
+
+  const { data, error } = useGeograph(center, distance / 1000.0);
+  useErrorToast("geograph-error", "Error loading Geograph images", error);
 
   return (
     <MarkerClusterGroup chunkedLoading showCoverageOnHover={false} removeOutsideVisibleBounds>
-      {data.map((item) => (
+      {data?.map((item) => (
         <Marker key={item.guid} position={[parseFloat(item.lat), parseFloat(item.long)]}>
           <ResultPopup
             title={item.title.replace(/.* : /, "")}
@@ -35,44 +33,4 @@ function Images({ latLng, distance }: ImagesProps) {
       ))}
     </MarkerClusterGroup>
   );
-}
-
-interface GeographLayerProps {
-  minZoom: number;
-}
-
-export function GeographLayer({ minZoom }: GeographLayerProps) {
-  const map = useMap();
-  const [settings] = useGeneralSettings();
-  const [latLng, setLatLng] = useState<LatLng>(map.getCenter());
-  const [overlayChecked, setOverlayChecked] = useState<Record<string, boolean>>({
-    Geograph: settings?.autoSelect?.geograph ?? false,
-  });
-
-  const handleOverlayChange = (layer: string, checked: boolean) => {
-    setOverlayChecked((prevState) => ({
-      ...prevState,
-      [layer]: checked,
-    }));
-  };
-
-  useMapEvents({
-    moveend() {
-      setLatLng(map.getCenter());
-    },
-    overlayadd(event) {
-      handleOverlayChange(event.name, true);
-    },
-    overlayremove(event) {
-      handleOverlayChange(event.name, false);
-    },
-  });
-
-  if (map.getZoom() < minZoom) {
-    return null;
-  }
-
-  const distance = map.distance(map.getCenter(), map.getBounds().getNorthEast()) * 2;
-
-  return <LayerGroup>{overlayChecked.Geograph && <Images latLng={latLng} distance={distance} />}</LayerGroup>;
 }
