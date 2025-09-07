8dda1ee2b25b865995cea158c7a03ac2c375316f
Richard Hull <rm_hull@yahoo.co.uk>
2025-09-07 01:25:33 +0100

    Removed unused

--- src/utils/settingsContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export interface SettingsContextProps {
-  open: boolean;
-  onOpen: () => void;
-  onClose: () => void;
-}
-
-export const SettingsContext = createContext<SettingsContextProps | undefined>(undefined);
