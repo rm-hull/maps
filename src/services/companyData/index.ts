4804ed82a8e5650367ad17b3f96478626b37e47f
Richard Hull <rm_hull@yahoo.co.uk>
2025-09-20 16:35:20 +0100

    Show when company accounts or confirmation statements are overdue (#716)
    * Show when company accounts or confirmation statements are overdue
    
    * Address PR comments
    
    * Jigging around
    
    * Dont fail the build when coveralls fails

--- src/services/companyData/index.ts
+++ src/services/companyData/index.ts
@@ -5,9 +5,20 @@ import { SearchResponse } from "./types";
 
 const API_URL = import.meta.env.VITE_COMPANY_DATA_API_URL as string;
 
+function dateReviver(_key: string, value: unknown) {
+  if (typeof value === "string") {
+    const iso8601 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z$/;
+    if (iso8601.test(value)) {
+      return new Date(value);
+    }
+  }
+  return value;
+}
+
 const client = axios.create({
   baseURL: API_URL,
   timeout: 10000,
+  transformResponse: (data: string) => JSON.parse(data, dateReviver) as SearchResponse,
 });
 
 export const fetchCompanyData = async (bounds: LatLngBounds): Promise<SearchResponse> => {
