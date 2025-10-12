3690d31509abba842cc0a3bbde38a1ad120bbb30
Richard Hull <rm_hull@yahoo.co.uk>
2025-10-12 21:39:00 +0100

    Fix linting issues

--- src/components/map/search/SearchResults.tsx
+++ src/components/map/search/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { Box, Listbox, createListCollection, Text, VStack } from "@chakra-ui/react";
+import { useCallback } from "react";
 import { GazetteerEntry, type Response } from "../../../services/osdatahub/types";
 import { SearchIcon } from "./SearchIcon";
 
@@ -11,45 +12,66 @@ interface SearchResponseProps {
   onSelect: (gazetteerEntry: GazetteerEntry) => void;
 }
 
+type SearchResultItemType = {
+  label: string;
+  value: string;
+  type: string;
+  icon: JSX.Element;
+  data: GazetteerEntry;
+};
+
+function SearchResultItem({ item, onSelect }: { item: SearchResultItemType; onSelect: (gazetteerEntry: GazetteerEntry) => void }) {
+  const handleClick = useCallback(() => {
+    onSelect(item.data);
+  }, [item.data, onSelect]);
+
+  return (
+    <Listbox.Item item={item} onClick={handleClick}>
+      <Box display="flex" alignItems="start" gap={3}>
+        <Box color="fg.muted" mt={0.5}>
+          {item.icon}
+        </Box>
+        <VStack alignItems="start" gap={0}>
+          <Listbox.ItemText>{item.label}</Listbox.ItemText>
+          <Text fontSize="xs" color="fg.muted">
+            {item.type}
+          </Text>
+        </VStack>
+      </Box>
+    </Listbox.Item>
+  );
+}
+
 export function SearchResults({ response, onSelect }: SearchResponseProps) {
   const collection = createListCollection({
     items:
-      response.results?.map((result) => ({
-        label: [
-          result.gazetteerEntry.name1,
-          result.gazetteerEntry.populatedPlace,
-          result.gazetteerEntry.countyUnitary,
-          result.gazetteerEntry.districtBorough,
-          result.gazetteerEntry.postcodeDistrict,
-          result.gazetteerEntry.country,
-        ]
-          .filter(Boolean)
-          .filter(dedupe)
-          .join(", "),
-        value: result.gazetteerEntry.id,
-        type: result.gazetteerEntry.localType,
-        icon: <SearchIcon localType={result.gazetteerEntry.localType} />,
-        data: result.gazetteerEntry,
-      })) ?? [],
+      response.results?.map(
+        (result) =>
+          ({
+            label: [
+              result.gazetteerEntry.name1,
+              result.gazetteerEntry.populatedPlace,
+              result.gazetteerEntry.countyUnitary,
+              result.gazetteerEntry.districtBorough,
+              result.gazetteerEntry.postcodeDistrict,
+              result.gazetteerEntry.country,
+            ]
+              .filter(Boolean)
+              .filter(dedupe)
+              .join(", "),
+            value: result.gazetteerEntry.id,
+            type: result.gazetteerEntry.localType,
+            icon: <SearchIcon localType={result.gazetteerEntry.localType} />,
+            data: result.gazetteerEntry,
+          } as SearchResultItemType)
+      ) ?? [],
   });
 
   return (
     <Listbox.Root collection={collection} width="500px">
       <Listbox.Content>
         {collection.items.map((item) => (
-          <Listbox.Item item={item} key={item.value} onClick={() => onSelect(item.data)}>
-            <Box display="flex" alignItems="start" gap={3}>
-              <Box color="fg.muted" mt={0.5}>
-                {item.icon}
-              </Box>
-              <VStack alignItems="start" gap={0}>
-                <Listbox.ItemText>{item.label}</Listbox.ItemText>
-                <Text fontSize="xs" color="fg.muted">
-                  {item.type}
-                </Text>
-              </VStack>
-            </Box>
-          </Listbox.Item>
+          <SearchResultItem key={item.value} item={item} onSelect={onSelect} />
         ))}
       </Listbox.Content>
     </Listbox.Root>
