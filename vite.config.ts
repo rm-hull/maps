e58063c3d009aedd909606b1df82714b893a4a60
Richard Hull <rm_hull@yahoo.co.uk>
2026-03-13 22:59:40 +0000

    Revert bad commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { execSync } from "child_process";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -9,7 +10,7 @@ export default defineConfig(() => {
   process.env.VITE_GIT_COMMIT_HASH = execSync("git describe --always --dirty").toString().trimEnd();
 
   return {
-    plugins: [react({ babel: { plugins: ["babel-plugin-react-compiler"] } })],
+    plugins: [react({ babel: { plugins: ["babel-plugin-react-compiler"] } }), tsconfigPaths()],
     base: "/maps",
     build: {
       sourcemap: true,
@@ -25,6 +26,5 @@ export default defineConfig(() => {
         exclude: ["src/**/*.spec.{ts,tsx}", "src/test/**", "**/*.d.ts"],
       },
     },
-    resolve: { tsconfigPaths: true }
   };
 });
